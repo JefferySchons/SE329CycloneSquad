eef142a4abfb34e9250773b48fcf8483b0a216ae
Chidike <chidi95@gmail.com>
2016-09-13 09:58:01 -0500

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,18 @@
+from Tkinter import *
+
+root = Tk()
+input = Entry()
+button1 = Button(text="Button")
+label = Label(text="Name")
+image = Label()
+text = Text()
+button2 = Button(text="Button")
+
+input.pack()
+button1.pack()
+label.pack()
+image.pack()
+text.pack()
+button2.pack()
+
+root.mainloop()
