cc4c548c258760d10a2b39f0954b4adad27fc3d9
David Boschwitz <hihid27@aim.com>
2016-09-13 11:40:32 -0500

    Fixed imports

--- DialogBox.py
+++ DialogBox.py
@@ -2,6 +2,8 @@
 # init with obj = DialogBox(tkRoot, "Label Text", "Submit Button Text"
 # then wait for its completion with root.wait_window(obj.top)
 # returns it's value to obj.value
+from Tkinter import *
+
 class DialogBox:
     def __init__(self, parent, labelText, submitText):
         top = self.top = Toplevel(parent)
