b8e4ff4a4c4c9b67c714bc9d5f79a5d81e452df3
David Boschwitz <hihid27@aim.com>
2016-09-13 11:33:06 -0500

    Create DialogBox.py

---
+++ DialogBox.py
@@ -0,0 +1,19 @@
+# creates a dialog box
+# init with obj = DialogBox(tkRoot, "Label Text", "Submit Button Text"
+# then wait for its completion with root.wait_window(obj.top)
+# returns it's value to obj.value
+class DialogBox:
+    def __init__(self, parent, labelText, submitText):
+        top = self.top = Toplevel(parent)
+        self.myLabel = Label(top, text=labelText)
+        self.myLabel.pack()
+
+        self.myEntryBox = Entry(top)
+        self.myEntryBox.pack()
+
+        self.mySubmitButton = Button(top, text=submitText, command=self.send)
+        self.mySubmitButton.pack()
+
+    def send(self):
+        self.value = self.myEntryBox.get()
+        self.top.destroy()
